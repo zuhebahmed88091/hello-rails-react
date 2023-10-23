0b61d078877b81eab5c359e380521cb26baf93ab
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2023-10-23 16:09:20 +0600

    Complete the backend

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  namespace :api do
+    namespace :v1 do
+      get '/greetings/random', to: 'greetings#random'
+    end
+  end
 end
